38991018e2335d90f8a907b11365e1b29700dcf1
José Alves <jose.alves@losch.lu>
2021-03-03 12:39:31 +0000

    added animation duration capability and optimized the code

--- lib/loading_skeleton.dart
+++ lib/loading_skeleton.dart
@@ -1,7 +1,35 @@
 library loading_skeleton;
 
 import 'package:flutter/material.dart';
-import 'package:loading_skeleton/animation_end.dart';
+
+// # Enum to define different ending points for the animation in the Y axis
+enum AnimationEnd {
+  EXTREMELY_ON_TOP,
+  MODERATELY_ON_TOP,
+  NORMAL,
+  MODERATELY_ON_BOTTOM,
+  EXTREMELY_ON_BOTTOM,
+}
+
+// # enum extension so we can define different values for the different enum index
+extension AnimationEndExtension on AnimationEnd {
+  double get value {
+    switch (this) {
+      case AnimationEnd.EXTREMELY_ON_TOP:
+        return -7;
+      case AnimationEnd.MODERATELY_ON_TOP:
+        return -3;
+      case AnimationEnd.NORMAL:
+        return 0;
+      case AnimationEnd.MODERATELY_ON_BOTTOM:
+        return 3;
+      case AnimationEnd.EXTREMELY_ON_BOTTOM:
+        return 7;
+      default:
+        return 0;
+    }
+  }
+}
 
 class LoadingSkeleton extends StatefulWidget {
   final double width;
@@ -9,20 +37,27 @@ class LoadingSkeleton extends StatefulWidget {
   final EdgeInsetsGeometry margin;
   final List<Color> colors;
   final AnimationEnd animationEnd;
+  final int animationDuration;
 
   // # This method constructs the LoadingSkeleton widget;
   // # Params [width] and [height] are @required;
   // # Params margin and colors are optional;
   // # If the user don't pass any colors list as param, the package will use a default set color list
-  // with shades of gray
+  // with shades of gray. #IMPORTANT: The colors list needs a size > 1 to work
+  // # Param [animationEnd] is optional and defines the animation ending point on Y axis.
+  // By default is set to "normal" => 0
+  // # Param [animationDuration] defines the duration of the animation in milliseconds.
+  // By default is set to 3000ms
   LoadingSkeleton({
     @required this.width,
     @required this.height,
     this.margin,
-    this.colors,
-    this.animationEnd,
+    this.colors = const [Colors.black12, Colors.black26, Colors.black12],
+    this.animationEnd = AnimationEnd.NORMAL,
+    this.animationDuration = 3000,
   })  : assert(width != null),
-        assert(height != null);
+        assert(height != null),
+        assert(colors.length > 1);
 
   @override
   _LoadingSkeletonState createState() => _LoadingSkeletonState();
@@ -37,7 +72,8 @@ class _LoadingSkeletonState extends State<LoadingSkeleton>
   void initState() {
     super.initState();
     _controller = AnimationController(
-        duration: Duration(milliseconds: 3000), vsync: this);
+        duration: Duration(milliseconds: widget.animationDuration),
+        vsync: this);
 
     gradientPosition = Tween<double>(begin: -3, end: 50).animate(
       CurvedAnimation(parent: _controller, curve: Curves.linear),
@@ -63,14 +99,11 @@ class _LoadingSkeletonState extends State<LoadingSkeleton>
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment(
-              gradientPosition.value,
-              widget.animationEnd != null
-                  ? widget.animationEnd.value
-                  : AnimationEnd.NORMAL.value),
+            gradientPosition.value,
+            widget.animationEnd.value,
+          ),
           end: Alignment(-10, 0),
-          colors: widget.colors != null && widget.colors.isNotEmpty
-              ? widget.colors
-              : [Colors.black12, Colors.black26, Colors.black12],
+          colors: widget.colors,
         ),
       ),
     );
